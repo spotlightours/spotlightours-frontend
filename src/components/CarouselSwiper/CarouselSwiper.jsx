eda1c8a91031f9b36399bbd0d0d1f31fa3f45713
Muhammad Arsalan <mianarsalan20@gmail.com>
2023-04-02 23:52:33 +0500

    Carousel Update

--- src/components/CarouselSwiper/CarouselSwiper.jsx
+++ src/components/CarouselSwiper/CarouselSwiper.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -12,6 +13,10 @@ import "./carouselSwiper.css";
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 
+import CarouselImg from "../.././assets/images/Spotlight.jpg";
+import CarouselImg1 from "../.././assets/images/Spotlight1.jpg";
+import CarouselImg2 from "../.././assets/images/Spotlight2.jpg";
+
 const CarouselSwiper = () => {
   return (
     <>
@@ -19,7 +24,7 @@ const CarouselSwiper = () => {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 5000,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -30,27 +35,36 @@ const CarouselSwiper = () => {
         className="swiper"
       >
         <SwiperSlide className="swiper-slide">
-          <img
-            src="https://www.linkpicture.com/q/Untitled-design-11_4.jpg"
-            alt=""
-          />
+          <img src={CarouselImg} alt="Best Desert Safaris" />
 
           <h1 className="slider__content__title">Best Desert Safaris</h1>
           <p className="slider__content__text">
             Don’t miss our popular Evening Desert Safari
-            <button className="slider__content__btn">Book Now</button>
+            <button className="slider__content__btn">
+              <Link to={`/tours/6423400c7826af276f798a5d`}>Book Now</Link>
+            </button>
           </p>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">
-          <img
-            src="https://www.linkpicture.com/q/Untitled-design-11_4.jpg"
-            alt=""
-          />
+          <img src={CarouselImg1} alt="Best Dubai Tours" />
 
-          <h1 className="slider__content__title">Best Desert Safaris</h1>
+          <h1 className="slider__content__title">Best Dubai Tours </h1>
           <p className="slider__content__text">
-            Don’t miss our popular Evening Desert Safari
-            <button className="slider__content__btn">Book Now</button>
+            Discover Dubai's magic with our tours
+            <button className="slider__content__btn">
+              <Link to={`/tours`}>Book Now</Link>
+            </button>
+          </p>
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide">
+          <img src={CarouselImg2} alt="" />
+
+          <h1 className="slider__content__title">Best Rental Service</h1>
+          <p className="slider__content__text">
+            Drive your way to adventure with our rental cars
+            <button className="slider__content__btn">
+              <Link to={`/cars`}>Book Now</Link>
+            </button>
           </p>
         </SwiperSlide>
       </Swiper>
